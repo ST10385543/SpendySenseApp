56b796c771b7ea16bebfae62c822a760eb866167
ST10306742 <st10306742@vcconnect.edu.za>
2025-05-01 22:40:18 +0200

    onClickCalc

--- app/src/main/java/com/example/spendysenseapp/CalculatorActivity.kt
+++ app/src/main/java/com/example/spendysenseapp/CalculatorActivity.kt
@@ -17,6 +17,4 @@ class CalculatorActivity : AppCompatActivity() {
             insets
         }
     }
-
-
 }
--- app/src/main/java/com/example/spendysenseapp/ui/AddTransaction/AddTransactionFragment.kt
+++ app/src/main/java/com/example/spendysenseapp/ui/AddTransaction/AddTransactionFragment.kt
@@ -1,31 +1,43 @@
 package com.example.spendysenseapp.ui.AddTransaction
 
-import androidx.fragment.app.viewModels
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.spendysenseapp.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.spendysenseapp.CalculatorActivity
+import com.example.spendysenseapp.Login
+import com.example.spendysenseapp.MainActivity
+import com.example.spendysenseapp.databinding.FragmentAddTransactionBinding
 
 class AddTransactionFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = AddTransactionFragment()
-    }
-
     private val viewModel: AddTransactionViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
+    private var _binding: FragmentAddTransactionBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_add_transaction, container, false)
+        _binding = FragmentAddTransactionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Set onClickListener for your ImageView
+        binding.imgCalc.setOnClickListener {
+            val intent = Intent(requireContext(), CalculatorActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
