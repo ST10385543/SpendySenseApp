93298f6d6536280466415271dfa2c318e673a82f
Tyron Jeremiah Naidoo <st10385722@vcconnect.edu.za>
2025-05-01 13:46:55 +0200

    XML files for rounded buttons - Home fragment semi done - Starting with recycler view for view all transactions

---
+++ app/src/main/java/com/example/spendysenseapp/Adapter/TransactionAdapter.kt
@@ -0,0 +1,13 @@
+package com.example.spendysenseapp.Adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.spendysenseapp.RoomDB.Transaction
+
+//class TransactionAdapter(private var transactionList: MutableList<Transaction>) :
+//    RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder>(){
+//
+//        //holds reference to each item UI elements
+//        class TransactionViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+//        }
+//}
