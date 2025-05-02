13a36e65c32e57685df9ef9c59eb89632d9f2e09
Tyron Jeremiah Naidoo <st10385722@vcconnect.edu.za>
2025-05-02 22:51:52 +0200

    - added toasters for edit and delete buttons

--- app/src/main/java/com/example/spendysenseapp/ui/ViewTransaction/ViewTransactionFragment.kt
+++ app/src/main/java/com/example/spendysenseapp/ui/ViewTransaction/ViewTransactionFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.spendysenseapp.Adapter.TransactionAdapter
@@ -56,6 +57,13 @@ class ViewTransactionFragment : Fragment() {
             loadTransactionData()
         }
         setupRecyclerView()
+
+        binding.editTransactionBtn.setOnClickListener{
+            Toast.makeText(requireContext(), "Not implemented yet!",Toast.LENGTH_LONG).show()
+        }
+        binding.deleteTransactionBtn.setOnClickListener{
+            Toast.makeText(requireContext(), "Not implemented yet!",Toast.LENGTH_LONG).show()
+        }
     }
     private fun setupRecyclerView(){
         transactionAdapter = TransactionAdapter(mutableListOf())
